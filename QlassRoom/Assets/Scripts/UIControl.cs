ee7bda791ed4f7bba588426ed9ac4173edaefeb7
Justus van Dijk <justusvdijk@gmail.com>
2017-10-05 12:32:19 +0200

    add: scene for development, ui script, started working on UI, added folders for images

---
+++ QlassRoom/Assets/Scripts/UIControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour
+{
+    public GameObject[] panels;
+    public GameObject activePanel;
+
+    public void OpenPanel(GameObject panel)
+    {
+        panel.SetActive(true);
+        activePanel = panel;
+        closeOtherPanels();
+    }
+
+    public void ClosePanel(GameObject panel)
+    {
+        panel.SetActive(false);
+    }
+
+    public void closeOtherPanels()
+    {
+        foreach (GameObject p in panels)
+        {
+            if(p != activePanel && p.active)
+            {
+                p.SetActive(false);
+            }
+        }
+    }
+
+}
